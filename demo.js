f1998940f8e76f149311399f5e871f4a9a539705
jmgarza94 <joey.garza94@gmail.com>
2020-07-07 15:20:30 -0700

    initial commits

---
+++ demo.js
@@ -0,0 +1,9 @@
+const http = require("http");  
+console.log('step 1')
+
+http.createServer((req,res) => {
+    console.log('step 2')
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hola Mundo');
+}).listen(process.env.PORT || 3000);
+console.log('step 3')
