346bf4c18d907bf538bab8fb9ae6fa7204c9feb5
jmgarza94 <joey.garza94@gmail.com>
2020-10-05 16:33:54 -0700

    React SPA delete and clear

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 
 app.use("/api", require("cors")()); // set Access-Control-Allow-Origin header for api route
 
-app.get("/", (req, res) => {
+//Browser User Interface Routes
+app.get("/oldHome", (req, res) => {
   return Cars.find({})
     .lean()
     .then((vehicles) => {
@@ -36,6 +37,38 @@ app.get("/detail", (req, res) => {
     .catch((err) => next(err));
 });
 
+app.get("/delete", (req, res) => {
+  //http://localhost:3000/delete?item=0
+  Cars.findOneAndDelete({ ID: req.query.item }, function (err, result) {
+    if (err) {
+      res.send("ERROR" + err);
+    } else {
+      console.log("success");
+      res.render("delete", { item: req.query.item });
+    }
+  });
+});
+
+// send plain text response
+app.get("/about", (req, res) => {
+  res.type("text/html");
+  res.sendFile(__dirname + "/public/about.html");
+  // res.type('text/plain');
+  // res.send('About page');
+});
+
+//React User Interface Routes
+app.get("/", (req, res) => {
+  return Cars.find({})
+    .lean()
+    .then((vehicles) => {
+      res.render('home_react', {vehicles: JSON.stringify(vehicles)});
+      // res.render("home", { vehicles });
+    })
+    .catch((err) => next(err));
+});
+
+//REST API
 app.get("/api/cars", (req, res) => {
   //http://localhost:3000/api/cars
   Cars.find({})
@@ -81,25 +114,9 @@ app.post("/api/cars", (req, res) => {
 });
 
 
-app.get("/delete", (req, res) => {
-  //http://localhost:3000/delete?item=0
-  Cars.findOneAndDelete({ ID: req.query.item }, function (err, result) {
-    if (err) {
-      res.send("ERROR" + err);
-    } else {
-      console.log("success");
-      res.render("delete", { item: req.query.item });
-    }
-  });
-});
+app.get("/about2", (req, res) => {
+  res.render("about2");
 
-
-// send plain text response
-app.get("/about", (req, res) => {
-  res.type("text/html");
-  res.sendFile(__dirname + "/public/about.html");
-  // res.type('text/plain');
-  // res.send('About page');
 });
 
 // define 404 handler
